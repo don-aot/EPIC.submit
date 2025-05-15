2ee0c6ef0fe19af359a7254acf0d6098855fee9c
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2025-05-15 09:38:01 -0700

    Update pre-hook-update-db.sh

--- submit-api/pre-hook-update-db.sh
+++ submit-api/pre-hook-update-db.sh
@@ -1,4 +1,17 @@
-#! /bin/sh
+#!/bin/sh
+
 cd /opt/app-root || exit
-echo 'starting upgrade'
+
+echo '--- Starting Alembic Upgrade ---'
+echo "PWD: $(pwd)"
+echo "Available Migration Scripts:"
+flask db history
+
+echo "Current DB Revision:"
+flask db current
+
+echo "Heads:"
+flask db heads
+
+echo "--- Running upgrade ---"
 flask db upgrade
